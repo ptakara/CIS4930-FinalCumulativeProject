6e1182a6589f9071bcd51f05bfc7ef13273db5a8
noahg24 <dethaystack24@gmail.com>
2026-05-02 18:44:29 -0400

    build: Cleaned up app.py's ports and tested docker successfully

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/health")
+@app.route("/api/health")
 def health():
     return jsonify({"status": "ok"}), 200
 
@@ -32,4 +32,4 @@ def result():
 
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
